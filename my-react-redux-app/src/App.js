97e90263fbdcf1302f75a0b38cc7bf1cdb5cc98d
salehmajeed <saleh.majeed8@gmail.com>
2021-10-14 09:53:28 +0530

    Updating the component

--- my-react-redux-app/src/App.js
+++ my-react-redux-app/src/App.js
@@ -1,15 +1,22 @@
+import React from 'react';
 import './App.css';
-import { Provider } from 'react-redux';
-import store from './component/reducer';
-import Counter from './component/counter';
+import { AddPostForm } from './features/posts/AddPostForm';
+import { EditPostForm } from './features/posts/EditPostForm';
+import { PostsList } from './features/posts/PostsList';
+import { SinglePostPage } from './features/posts/SinglePostPage';
+// import Counter from './component/counter';
 
 function App() {
 	return (
 		<div className='App'>
-			<Provider store={store}>
-				{/* Centralized State for child Components */}
-				<Counter initialInputValue={2} />
-			</Provider>
+			{/* Centralized State for child Components */}
+			{/* <Counter initialInputValue={2} /> */}
+			<React.Fragment>
+				<SinglePostPage match={{ params: { postId: 1 } }} />
+				<EditPostForm match={{ params: { postID: 1 } }} />
+				<AddPostForm />
+				<PostsList />
+			</React.Fragment>
 		</div>
 	);
 }
