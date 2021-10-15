dea7acffadfddb2a368e93a0d00192daf7b712ab
salehmajeed <saleh.majeed8@gmail.com>
2021-10-15 20:53:47 +0530

    notification slice

---
+++ my-react-redux-app/src/features/notifications/notificationsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
+
+export const fetchNotifications = createAsyncThunk('notifications/fetchNotifications', async (_, { getState }) => {
+	const allNotifications = selectAllNotifications(getState());
+	const [lastestNotification] = allNotifications;
+	const latestTimeStamp = lastestNotification ? lastestNotification.date : '';
+	const response = await client.get(`fakeApi/notifications?since=${latestTimeStamp}`);
+	return response.data;
+});
+
+const notificationsSlice = createSlice({
+	name: 'notifications',
+	initialState: [],
+	reducers: {
+		allNotificationsRead(state, action) {
+			state.forEach(notification => {
+				notification.read = true;
+			});
+		},
+	},
+	extraReducers(builder) {
+		builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+			state.push(...action.payload);
+			state.forEach(notification => {
+				notification.isNew = !notification.read;
+			});
+			state.sort((a, b) => b.date.localeCompare(a.date));
+		});
+	},
+});
+
+export const { allNotificationsRead } = notificationsSlice.actions;
+export default notificationsSlice.reducer;
+export const selectAllNotifications = state => state.notifications;
