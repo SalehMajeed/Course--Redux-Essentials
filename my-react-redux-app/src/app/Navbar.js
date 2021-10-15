6b945c28c62b5374130a15768975dbf03f1d54c3
salehmajeed <saleh.majeed8@gmail.com>
2021-10-15 20:55:00 +0530

    modified files

--- my-react-redux-app/src/app/Navbar.js
+++ my-react-redux-app/src/app/Navbar.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { fetchNotifications, selectAllNotifications } from '../features/notifications/notificationsSlice';
 
 export const Navbar = () => {
+	const dispatch = useDispatch();
+	const notifications = useSelector(selectAllNotifications);
+	const numUnreadNotification = notifications.filter(n => !n.read).length;
+
+	let numUnreadNotificationBadge;
+	if (numUnreadNotification > 0) {
+		numUnreadNotificationBadge = <span className='badge'> {numUnreadNotification}</span>;
+	}
+
+	const fetchNewNotifications = () => {
+		dispatch(fetchNotifications());
+	};
 	return (
 		<nav>
 			<section>
@@ -9,7 +23,12 @@ export const Navbar = () => {
 				<div className='navContent'>
 					<div className='navLinks'>
 						<Link to='/'>Posts</Link>
+						<Link to='/users'>Users</Link>
+						<Link to='/notifications'>Notifications{numUnreadNotificationBadge}</Link>
 					</div>
+					<button className='button' onClick={fetchNewNotifications}>
+						Refresh Notifications
+					</button>
 				</div>
 			</section>
 		</nav>
--- my-react-redux-app/src/app/store.js
+++ my-react-redux-app/src/app/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import notificationsReducer from '../features/notifications/notificationsSlice';
 import postsReducer from '../features/posts/postSlice';
 import userReducer from '../features/users/userSlice';
 
@@ -6,5 +7,6 @@ export default configureStore({
 	reducer: {
 		posts: postsReducer,
 		users: userReducer,
+		notifications: notificationsReducer,
 	},
 });
